6ee1fca97bcce468bd8f56c5ab204a9dd1b455ee
xuanngoc <bx.ngoc.99@gmail.com>
2020-05-05 02:06:21 +0700

    add garden beans

---
+++ src/main/java/xuanngoc/gardenwatersystem/repository/PlantRepository.java
@@ -0,0 +1,17 @@
+package xuanngoc.gardenwatersystem.service;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import xuanngoc.gardenwatersystem.model.Plant;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+import java.util.List;
+
+@Repository
+public interface PlantRepository extends JpaRepository<Plant, Integer> {
+
+
+}
